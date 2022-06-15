84d1608a6cc4543318efaa35fc9f862b1eec7adf
Jose Moreno <zamudiomorenojose1@gmail.com>
2022-06-15 02:08:35 -0500

    Trabajando con templates

--- prueba/inicio/views.py
+++ prueba/inicio/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render, HttpResponse
 
 menu="""
 <a href="/">Home</a>
+<a href="/formulario">Registrar</a>
 <a href="/contacto">Contactanos</a>"""
 
 def principal(request):
-    contenido = "<h1>HOLA DJANGO</h1>"
-    return HttpResponse(menu + contenido)
+    return render(request, "inicio/principal.html")
 
 def contacto(request):
     contenido = """<h2>Contacto<h2>
--- prueba/prueba/urls.py
+++ prueba/prueba/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.principal, name="Principal"),
     path('contacto/',views.contacto, name="Contacto"),
+    path('formulario/',views.contacto, name="Formulario"),
 ]
