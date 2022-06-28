727fe09d7e3cc849984803c918e8a46e4a84cd55
Jose Moreno <zamudiomorenojose1@gmail.com>
2022-06-28 00:44:59 -0500

    Registro

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Alumnos
 
 # Register your models here.
+
+class AdministrarModelo(admin.ModelAdmin):
+    readonly_fields = ('created', 'update')
+
+admin.site.register(Alumnos, AdministrarModelo)
--- prueba/registros/models.py
+++ prueba/registros/models.py
@@ -1,12 +1,22 @@
+from tabnanny import verbose
 from turtle import update
 from django.db import models
 
 # Create your models here.
 class Alumnos(models.Model): #Define la estructura de nuestra tabla
-    matricula = models.CharField(max_length=12)#Texto corto
+    matricula = models.CharField(max_length=12,verbose_name="Mat")#Texto corto
     nombre = models.TextField()#Texto largo
     carrera = models.TextField()
     turno = models.CharField(max_length=10)
     created = models.DateTimeField(auto_now_add=True)#Fecha y tiempo
     update = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "Alumno"
+        verbose_name_plural = "Alumnos"
+        ordering = ["-created"]
+        #el menos indica que se ordenara del mas reciente al mas viejo
+
+    def __str__(self):
+        return self.nombre
+            #Indica que se mostrara el nombre como valor en la tabla
